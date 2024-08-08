63495f54c787d10eadc348858827eaaeab029716
rajat459 <nandi00rajat@gmail.com>
2024-08-08 12:18:34 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,322 @@
+//1.add two number 34+78 
+ #include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    printf("enter the value of a=");
+    scanf("%d",&a);
+    printf("enter the value of b=");
+    scanf("%d",&b);
+        c=a+b;
+    printf("%d",c);
+    return 0;
+}
+
+—---------------------------------------------------------------------------------------------
+//Subtract two number
+ #include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    printf("enter the value of a=");
+    scanf("%d",&a);
+    printf("enter the value of b=");
+    scanf("%d",&b);
+        c=a-b;
+    printf("%d",c);
+    return 0;
+}
+—------------------------------------------------------------------------------------------------------------------------
+//Multiple equation theory
+
+#include <stdio.h>
+
+int main()
+{
+    float a=8.9, b=6.67 , c=6.21, d=5.6, e=7.8, h;
+    int f=34, g=157 ;
+    h=a*b-(c-d)+e-f+g;
+    printf("%f",h);
+   
+
+
+    return 0;
+}
+—------------------------------------------------------------------------------------------------------------------------
+
+//Write a program to check the number is greater than 10 or not
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b ;
+    printf("enter the value of a=");
+    scanf("%d",&a);
+     if (a>10)
+     {
+         printf("num is greater than 10");
+     }
+	else{ 
+	
+		printf("num is less than");
+		}
+
+    return 0;
+}
+
+—-------------------------------------------------------------------------------------------
+//Write a program to  check the number is greater than 50 or not
+#include <stdio.h>
+
+int main()
+{
+    int a, b ;
+    printf("enter the value of a=");
+    scanf("%d",&a);
+     if (a>50)
+     {
+         printf("num is greater than 50");
+     }
+	else{ 
+	
+		printf("num is less than 50");
+		}
+
+    return 0;
+}
+—--------------------------------------------------------------------------------------------------------
+//Write a program to  check the number is greater than 45 or not
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b ;
+    printf("enter the value of a=");
+    scanf("%d",&a);
+     if (a>50)
+     {
+         printf("num is greater than 50");
+     }
+	else{ 
+	
+		printf("num is less than 50");
+		}
+    return 0;
+}
+—----------------------------------------------------------------------------------------------------------------
+//Write a program to check a user is valid or not for voting
+#include <stdio.h>
+
+int main()
+{
+    int age,  ;
+    printf("enter the  age=");
+    scanf("%d",&age);
+     if (a>18)
+     {
+         printf("eligible for voting");
+     }
+	else{ 
+	
+		printf("not eligible");
+		}
+
+    return 0;
+}
+—----------------------------------------------------------------------------------------------------------------
+//Check a number is even or odd
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   printf("enter the value of a=");
+   scanf("%d",&a);
+   if(a%2==0)
+   {
+       printf("number is even");
+       
+   }
+   else{
+       printf("number is odd");
+   }
+
+    return 0;
+}
+
+—-------------------------------------------------------------------------------------------------------
+//Write a c program and check a number is divisible by  5
+
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   printf("enter the value of a=");
+   scanf("%d",&a);
+   if(a%5==0)
+   {
+       printf("number is even");
+       
+   }
+   else{
+       printf("number is odd");
+   }
+
+    return 0;
+}
+
+-----------------------------------------------------------------------------------------------------
+//Write a program input character ‘a’ then print apple otherwise input is not valid
+#include <stdio.h>
+
+int main()
+{
+  char a;
+  printf("enter the character");
+  scanf("%c",&a);
+  if (a='a')
+  {
+      printf("apple");
+      
+  }
+  else
+  {
+      printf("input is not valid");
+  }
+
+    return 0;
+}
+
+---------------------------------------------------------------
+//Write a program input character ‘h’ then print house  otherwise input is not valid
+#include <stdio.h>
+
+int main()
+{
+  char a;
+  printf("enter the character");
+  scanf("%c",&a);
+  if (a='h')
+  {
+      printf("house");
+      
+  }
+  else
+  {
+      printf("input is not valid");
+  }
+
+    return 0;
+}
+
+---------------------------------------------------------------
+
+//Check a input is vowel or consonant
+
+#include <stdio.h>
+
+int main()
+{
+  char a;
+  printf("enter the character");
+  scanf("%c",&a);
+  if (a=='a'|| a=='e'|| a=='i' || a=='o' || a=='u')
+  {
+      printf("vowel");
+      
+  }
+  else
+  {
+      printf("consonant");
+  }
+
+    return 0;
+}
+--------------------------------------
+//write a c program input character when user input p , e , a , k peak word other wise input is not valid
+
+#include <stdio.h>
+
+int main()
+{
+  char a;
+  printf("enter the character");
+  scanf("%c",&a);
+  if (a=='p'|| a=='e'|| a=='a' || a=='k')
+  {
+      printf("peak");
+      
+  }
+  else
+  {
+      printf("invalid");
+  }
+
+    return 0;
+}
+
+
+—-----------------------------------------------------------------------------------------------------------------------
+//write a c program input character when user input q,w,e,r,t,y,u,i,o,p upper line other wise input is not valid
+
+#include <stdio.h>
+
+int main()
+{ 
+  char a;
+  printf("enter the character");
+  scanf("%c",&a);
+  if (a=='q'|| a=='w'|| a=='e' || a=='r'|| a==’t'|| a=='y'|| a=='u' || a=='i')
+  {
+      printf("upper line");
+      
+  }
+  else
+  {
+      printf("invalid");
+  }
+
+    return 0;
+}
+
+
+//write a c program input number when user input a number 1 then print one other wise input is not valid
+
+
+#include <stdio.h>
+
+int main()
+{
+  Int input;
+  printf("enter the input");
+  scanf("%d",&input);
+  if (input==’1’)
+  {
+      printf("1");
+      
+  }
+  else
+  {
+      printf("invalid");
+  }
+
+    return 0;
+}
+
+—--------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+   
